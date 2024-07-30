2296ed630adc36c164cf87b877cd33e1d938608f
kad <footnofetish123@gmail.com>
2024-07-29 21:00:12 -0400

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,13 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-brown-orange": "linear-gradient(to top right, #8B4513, #FFA500)",
+        "gradient-orange": "linear-gradient(to top right, #FDBA74, #FB923C, #F97316)", // This matches the bg-gradient-to-tr from-orange-200 via-orange-300 to-orange-400
       },
     },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [""],
+  },
 };
